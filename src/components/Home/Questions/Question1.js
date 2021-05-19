a6ac3a819d3361f78cdea7744e363e524acb7e47
Abdify <abdullah.fahimm08@gmail.com>
2021-05-19 22:45:10 +0600

    questions animation added

--- src/components/Home/Questions/Question1.js
+++ src/components/Home/Questions/Question1.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './Questions.css';
 const Question1 = () => {
     return (
-        <div>
-            <h1>How Do You Approach A Tough Task?</h1>
+        <div className="questions question1">
+            {/* <h1>How Do You Approach A Tough Task?</h1> */}
             <h3>DIVIDE & RULE</h3>
             <i>That's why I love React!</i>
         </div>
